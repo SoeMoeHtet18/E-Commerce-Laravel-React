f879fe9a5c956d0b70fb92de1eb468e10b1834ab
SMH <soemoehtet18203@gmail.com>
2024-04-15 12:35:07 +0630

    deploy

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,11 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/home.js', 'public/js').react();
-mix.js('resources/js/productDetail.js', 'public/js').react();
-mix.js('resources/js/profile.js', 'public/js').react();
+mix.js('resources/js/app.js', 'public/js')
+    .react()
+    .sass('resources/sass/app.scss', 'public/css');
